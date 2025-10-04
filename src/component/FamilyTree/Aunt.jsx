a9a126a38806dd1fc28cde783d672b6ccdb6333c
Dibakar-Biswas <dibakarbiswas742@gmail.com>
2025-10-05 00:57:50 +0600

    All Done

--- src/component/FamilyTree/Aunt.jsx
+++ src/component/FamilyTree/Aunt.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { use } from 'react';
 import Cousin from './Cousin';
+import { MoneyContext } from './FamilyTree';
 
 const Aunt = ({asset}) => {
+    const [money, setMoney] = use(MoneyContext)
+
+    const handleAddMoney = () => {
+        setMoney(money + 5000)
+    }
     return (
         <div>
             <h3>Aunt</h3>
             <section style={{display: 'flex'}}>
                 <Cousin asset={asset} name="Putul Di"></Cousin>
-                <Cousin name="Samir Da"></Cousin>
+                <Cousin name="Samir Da"></Cousin>\
+                <button onClick={handleAddMoney}>Add 5000 tk</button>
             </section>
         </div>
     );
